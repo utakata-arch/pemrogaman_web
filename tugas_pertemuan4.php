f0633297deefa941a59c7c6c6e9cd9dfe6258567
utakata-arch <tagam874@gmail.com>
2026-04-16 21:22:06 +0700

    Add files via upload

---
+++ tugas_pertemuan4.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th colspan="4">Daftar Smartphone</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama Brand</th>
+            <th>Gambar</th>
+            <th>Keterangan</th>
+        </tr>
+        <tr>
+           
+            <tr>
+                 <td rowspan="3">1</td>
+                <td rowspan="3">Apple</td>
+                <td rowspan="3"><img src="foto/IP26.png" , alt="ip", width="100"></td>
+                <td>17 Pro Max 1Tb Deep Blue</td>
+            <tr>
+                <td>Layar 6,9 inci yang cemerlang1, desain unibody aluminium, chip A19 Pro, semua kamera belakang 48 MP</td>
+            <tr>
+                <td>Rp 36.000.000</td>
+             </tr>
+                
+            </tr>
+            <tr>
+                 <td rowspan="3">2</td>
+                <td rowspan="3">Samsung</td>
+                <td rowspan="3"><img src="foto/Samsung S24 Ultra.jpg" , alt="samsung", width="100"></td>
+                <td>Galaxy S24 Ultra</td>
+            <tr>
+                <td> HP Premium dengan layar besar, S Pen, Kamera Canggih, dan performa sangat mewah</td>
+            <tr>
+                <td>Rp 18.000.000</td>
+            </tr>
+             <tr>
+                <td rowspan="3">3</td>
+                <td rowspan="3">Xiaomi</td>
+                <td rowspan="3"><img src="foto/Xiaomi 12.jpg" , alt="xiaomi", width="100"></td>
+                <td>Xiaomi 12 8/256</td>
+            <tr>
+                <td> HP flaghsip dengan performa kencang, design elegan, dengan harga terjangkau</td>
+            <tr>
+                <td>Rp 8.000.000</td>
+            </tr>
+        </tr>
+       
+            
+
+           
+            
+        
+
+    </table>
+    
+</body>
+</html>
